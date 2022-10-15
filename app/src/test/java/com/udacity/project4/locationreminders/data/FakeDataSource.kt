e083476c98bda513eb289ebe839b4899e1e1a229
mohamedadel2323 <mohamedadel2323m@gmail.com>
2022-10-15 21:56:04 +0200

    FakeDataSource issue solved

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -6,7 +6,7 @@ import com.udacity.project4.locationreminders.data.dto.Result.Success
 import com.udacity.project4.locationreminders.data.dto.Result.Error
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource(var reminders: MutableList<ReminderDTO>? = mutableListOf()) :
+class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()) :
     ReminderDataSource {
     private var shouldReturnError = false
 
@@ -16,35 +16,35 @@ class FakeDataSource(var reminders: MutableList<ReminderDTO>? = mutableListOf())
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
         try {
-            if (shouldReturnError){
+            if (shouldReturnError) {
                 return Error("Test Error")
             }
-            reminders?.let { return Success(ArrayList(it)) }
-            return Error(" not found.")
-        }catch (e : Exception){
+            return Success(ArrayList(reminders))
+        } catch (e: Exception) {
             return Error(e.localizedMessage)
         }
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        reminders?.add(reminder)
+        reminders.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
         try {
-            val result = reminders?.firstOrNull { it.id == id }
+            if (shouldReturnError) {
+                return Error("Test Error")
+            }
+            val result = reminders.firstOrNull { it.id == id }
             result?.let {
                 return Success(it)
             }
             return Error("Reminder Not Found")
-        }catch (e : Exception){
+        } catch (e: Exception) {
             return Error(e.localizedMessage)
         }
-
-
     }
 
     override suspend fun deleteAllReminders() {
-        reminders?.clear()
+        reminders.clear()
     }
 }
